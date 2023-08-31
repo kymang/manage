b0cd04a796be3d43183483829daa2f7906ea10ef
kymang <140633527+kymang@users.noreply.github.com>
2023-08-31 16:04:37 +0700

    Update callses.py

--- Amang/modules/callses.py
+++ Amang/modules/callses.py
@@ -68,7 +68,7 @@ Jika menemukan Kendala atau Masalah, silahkan hubungi @amwangsupport atau @amwan
                     callback_data="help_bot"),
                   InlineKeyboardButton(
                     text"Ubot Premium",
-                    callback_data="bahan")
+                    callback_data="bahan"),
                 ],
                 [
                   InlineKeyboardButton(
